7d06b66ba579b9aef9bf4e930d5e2c8c3efddea0
ad0ll <20057155+ad0ll@users.noreply.github.com>
2025-07-04 18:38:44 -0400

    Change contracts to typescript

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,16 @@
+import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 100000,
+      },
+    },
+  },
+};
+
+export default config;
